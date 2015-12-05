d2e216bbeb3db085c80d74ad05942367269b65d6
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-12-05 16:33:13 +0200

    get location

---
+++ server.js
@@ -0,0 +1,29 @@
+/*** REQUIRES ****************************************************************/
+
+var express = require('express');
+
+/*****************************************************************************/
+
+
+
+/*** SERVER BEHAVIOUR ********************************************************/
+
+var app = express();
+app.use(express.static('public'));
+
+app.set('port', (process.env.PORT || 5000));
+
+/* Main page */
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/" + "index.html" );
+});
+
+/* Start server */
+var server = app.listen(app.get('port'), function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Server is up and running at http://%s:%s", host, port);
+})
+
+/*****************************************************************************/
