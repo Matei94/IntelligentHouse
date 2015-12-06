6a025ba16064389ef87defba2f2613f6d12b7c6e
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-12-06 13:59:06 +0200

    location and wheather

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 /*** VARIABLES ***************************************************************/
 
 var gOpenWheatherApiKey = '98c7e7ff6542a39ef0a58614185c8031';
+var gPosition = null;
 
 /*****************************************************************************/
 
@@ -10,6 +11,36 @@ var gOpenWheatherApiKey = '98c7e7ff6542a39ef0a58614185c8031';
 
 $(document).ready(function() {
     getLocation();
+
+    $('#locationBTN').click(function() {
+        setTimeout(function() {
+            showPosition(gPosition);
+        }, 1000);
+    });
+
+    $('#wheatherBTN').click(function() {
+        if (gPosition == null) {
+            alert("Position is not available");
+        } else {
+            $.ajax({
+                url: 'http://api.openweathermap.org/data/2.5/weather?' +
+                     'lat=' + gPosition.coords.latitude +
+                     '&lon=' + gPosition.coords.longitude +
+                     '&APPID=' + gOpenWheatherApiKey,
+                dataType: 'jsonp',
+                success: function(results){
+                    new PNotify({
+                        title: 'Weather',
+                        text: JSON.stringify(results),
+                        type: 'info',
+                        animate_speed: 'fast',
+                        hide: true,
+                        delay: 2000
+                    });
+                }
+            });
+        }
+    });
 });
 
 /*****************************************************************************/
@@ -19,35 +50,32 @@ $(document).ready(function() {
 /*** FUNCTIONS ***************************************************************/
 
 function getLocation() {
-    var x = document.getElementById("demo");
-
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(setPosition, showError);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
+}
+
+
+function setPosition(position) {
+    gPosition = position;
+
+    new PNotify({
+        title: 'Location available',
+        text: 'Latitude: ' + gPosition.coords.latitude +
+              '\nLongitude: ' + gPosition.coords.longitude,
+        type: 'success',
+        animate_speed: 'fast',
+        hide: true,
+        delay: 2000
+    });
 }
 
 
 function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-
-    $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon +
-             '&APPID=' + gOpenWheatherApiKey,
-        dataType: 'jsonp',
-        success: function(results){
-            new PNotify({
-                title: 'Weather',
-                text: JSON.stringify(results),
-                type: 'info',
-                animate_speed: 'fast',
-                hide: true,
-                delay: 2000
-            });
-        }
-    });
 
     latlon = new google.maps.LatLng(lat, lon)
     mapholder = document.getElementById('mapholder')
@@ -67,20 +95,18 @@ function showPosition(position) {
 
 
 function showError(error) {
-    var x = document.getElementById("demo");
-
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
+            alert("User denied the request for Geolocation.");
             break;
         case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
+            alert("Location information is unavailable.");
             break;
         case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
+            alert("The request to get user location timed out.");
             break;
         case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
+            alert("An unknown error occurred.");
             break;
     }
 }
